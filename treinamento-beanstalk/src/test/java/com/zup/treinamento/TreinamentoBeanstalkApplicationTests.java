7f43c228ad93bcf1b8d4b64afa1ad26de73a29be
carloslopeszup <carlos.lopes@zup.com.br>
2021-05-13 19:06:28 -0300

    primeiro commit

---
+++ treinamento-beanstalk/src/test/java/com/zup/treinamento/TreinamentoBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
